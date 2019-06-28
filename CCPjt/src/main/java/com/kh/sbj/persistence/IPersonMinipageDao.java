f6529493591d32f094b6dc85b08fc4b0de3e1523
손병진 <sbj5757@naver.com>
2019-06-28 11:23:21 +0900

    06-28-sbj

--- CCPjt/src/main/java/com/kh/sbj/persistence/IPersonMinipageDao.java
+++ CCPjt/src/main/java/com/kh/sbj/persistence/IPersonMinipageDao.java
@@ -2,9 +2,12 @@ package com.kh.sbj.persistence;
 
 import java.util.List;
 
+import com.kh.sbj.domain.PersonPromiseDeleteDto;
 import com.kh.sbj.domain.PersonPromiseVo;
 
 public interface IPersonMinipageDao {
 	public List<PersonPromiseVo> selectAllPromise(String u_id) throws Exception;
 	public void insertPromise(PersonPromiseVo personPromiseVo) throws Exception;
+	public void updatePromise(PersonPromiseVo personPromiseVo) throws Exception;
+	public void deletePromise(PersonPromiseDeleteDto deleteDto) throws Exception;
 }
--- CCPjt/src/main/java/com/kh/sbj/persistence/PersonMinipageDaoImpl.java
+++ CCPjt/src/main/java/com/kh/sbj/persistence/PersonMinipageDaoImpl.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
+import com.kh.sbj.domain.PersonPromiseDeleteDto;
 import com.kh.sbj.domain.PersonPromiseVo;
 @Repository
 public class PersonMinipageDaoImpl implements IPersonMinipageDao {
@@ -17,13 +18,24 @@ public class PersonMinipageDaoImpl implements IPersonMinipageDao {
 	@Override
 	public List<PersonPromiseVo> selectAllPromise(String u_id) throws Exception {
 		List<PersonPromiseVo> list = sqlSession.selectList("person_promise.selectAllPromise", u_id);
-		System.out.println("DAO LIST = " + list);
 		return list;
 	}
 
 	@Override
 	public void insertPromise(PersonPromiseVo personPromiseVo) throws Exception {
 		sqlSession.insert("person_promise.insertPromise", personPromiseVo);
+		
+	}
+
+	@Override
+	public void updatePromise(PersonPromiseVo personPromiseVo) throws Exception {
+		sqlSession.update("person_promise.updatePromise", personPromiseVo);
+		
+	}
+
+	@Override
+	public void deletePromise(PersonPromiseDeleteDto deleteDto) throws Exception {
+		sqlSession.delete("person_promise.deletePromise", deleteDto);
 		
 	}
 
