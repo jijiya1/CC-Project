ef9e138196d96c5b528bd077260f5a6dd265f8d5
YongSeck <ahems2005@naver.com>
2019-07-03 11:21:38 +0900

    1. 토론 주제 추천 게시판 조회 2. 조회수 증가 3. 삭제 기능 추가

--- CCPjt/src/main/java/com/kh/hys/service/ISelectService_Discussion.java
+++ CCPjt/src/main/java/com/kh/hys/service/ISelectService_Discussion.java
@@ -11,6 +11,15 @@ public interface ISelectService_Discussion {
 	public List<SelectDiscussion_BoardVo> getSelectBoardList(PagingDto pagingDto,int b_addInfo) throws Exception;
 	
 	// 토론 주제 추천 게시판 글갯수 가져오기
-	public int totalSelectBoardCount(int b_addInfo) throws Exception;
+	public int totalSelectBoardCount(PagingDto pagingDto,int b_addInfo) throws Exception;
+	
+	// 글 상세보기
+	public SelectDiscussion_BoardVo readSelectBoard(int b_no) throws Exception;
+	
+	// 조회수 증가
+	public void addReadCount(int b_no) throws Exception;
+	
+	// 글 삭제 하기 (b_checkeddel = 0 -> 1 로 업데이트)
+	public void deleteSelectBoard(int b_no) throws Exception;
 
 }
